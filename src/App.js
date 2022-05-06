94d18faa869b31eeeb9508f4f00c430b30011436
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-05-06 14:24:44 +0600

    home component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       <Header></Header>
       <Routes>
-        
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
